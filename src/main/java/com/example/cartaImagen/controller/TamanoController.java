b06c4f6afa872a033fd5a24c6f9f53d66fa07b39
anapalacio0721 <anapalacio0721@gmail.com>
2022-10-27 08:37:02 -0500

    Mensaje Ana

--- src/main/java/com/example/cartaImagen/controller/TamanoController.java
+++ src/main/java/com/example/cartaImagen/controller/TamanoController.java
@@ -19,7 +19,6 @@ public class TamanoController {
     @PostMapping
     public ResponseEntity<Tamano> create(@RequestBody Tamano tamano){
         System.out.println("Primera persona del cambio Andrea");
-        System.out.println("Segunda persona del cambio  Marce");
         return ResponseEntity.ok(cartaService.ajustarTamano(tamano));
     }
 }
